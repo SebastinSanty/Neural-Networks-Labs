e897bcde4238c1b986276d2b90dab1a8378558a9
Wazeer Zulfikar <wazeer.zulfikar@gmail.com>
2017-10-17 19:32:56 +0530

    Half trainable densenet

--- nnfl.py
+++ nnfl.py
@@ -6,6 +6,7 @@ class Graph:
         self.num = 0
         self.layers = list()
         self.values_dim = values_dim
+        self.lr = 0.9
 
 
     def addgate(self, activation, units=0):
@@ -31,6 +32,7 @@ class Graph:
             
         layer.weights = np.random.rand(prev_units,units)
         layer.activation = act_layer
+        layer.units = units
 
         self.layers.append(layer)
         self.num =  self.num + 1
@@ -40,6 +42,7 @@ class Graph:
         values = values.reshape(self.values_dim,1)
 
         for i in range (self.num):
+            self.layers[i].input_values = values
             values = np.dot(np.transpose(self.layers[i].weights), values)
             self.layers[i].predicted_values = self.layers[i].activation.forward(values)
             values = self.layers[i].predicted_values
@@ -49,16 +52,16 @@ class Graph:
     def backward(self, expected, predicted):
         error = expected - predicted
         for i in range (self.num,0,-1):
-            for j in range(0, self.layers[i-1].units):
-                self.layers[i-1].delta[j] = error[j] * self.layers[i-1].activation.backward(self.layers[i-1].predicted_values[j])
+            del_activation = self.layers[i-1].activation.backward(self.layers[i-1].predicted_values)
+            self.layers[i-1].delta = np.dot(error,del_activation)
             error = np.sum(self.layers[i-1].delta * np.transpose(self.layers[i-1].weights))
 
+        return error
 
     def update(self, loss):
         for i in range(self.num, 0, -1):
-            for j in range(0, self.layers[i-1].units):
-                self.layers[i-1].weights[j] = self.layers[i-1].weights[j] + lr*self.layers[i-1].delta*self.layers[i-2].predicted_values + mf*self.layers[i-1].del_w[j]
-                self.layers[i-1].del_w[j] = lr*self.layers[i-1].delta * self.layers[i-2].predicted_values
+            self.layers[i-1].weights = self.layers[i-1].weights + self.lr*np.dot(self.layers[i-1].delta,self.layers[i-1].input_values)# + mf*self.layers[i-1].del_w[j]
+            #self.layers[i-1].delta = self.lr*np.dot(self.layers[i-1].delta,self.layers[i-2].predicted_values)
 
 class Layer(dict):
     pass
@@ -72,9 +75,9 @@ class ReLU:
             return values
         return 0
     def backward(self, dz):
-        if(dz > 0):
-            return 1
-        return 0
+        delta = np.zeros(dz.shape)
+        delta[np.where(dz>0)] = 1
+        return delta
 
 class Sigmoid:
 # Sigmoid
@@ -118,10 +121,11 @@ class DenseNet:
         self.network.addgate(activation, units)
 
     def train(self, X, Y):
-        predicted = self.network.forward(X)
-        loss = self.network.backward(Y,predicted)
-        self.network.update(loss)
-        return loss_value
+        for x,y in zip(X,Y):
+            predicted = self.network.forward(x)
+            loss = self.network.backward(y,predicted)
+            self.network.update(loss)
+        return loss
 
     def predict(self, X):
         output = list()
--- test.py
+++ test.py
@@ -4,4 +4,18 @@ from nnfl import *
 a = DenseNet(2,5,"a")
 a.addlayer("Sigmoid",1)
 
-print(a.predict(np.array([[1,2],[5,6]])))
+X = np.array([[0,0],[0,1],[1,0],[1,1]])
+y = np.array([[0],[0],[0],[1]])
+
+x1 = np.array([[0,0]])
+y1 = np.array([[0]])
+
+print(a.predict(x1))
+
+print("Training..")
+for i in range(100):
+	a.train(x1,y1)
+
+print("Testing..")
+
+print(a.predict(np.array(x1)))
